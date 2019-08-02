f176f6b6fc9aad78d9a95b7948caad19caf3fccd
BEa45xz <49711174+BEa45xz@users.noreply.github.com>
2019-08-02 13:05:55 +0200

    Update Package.swift
    dependencies' must precede argument 'targets'

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@ import PackageDescription
 
 let package = Package(
   name:         "SwiftClickHouse",
-  targets:      [
-      .target(name: "SwiftClickHouse", dependencies: ["SwiftyJson", "BlueSocket"]),
-  ],
   dependencies: [
       .package(url: "https://github.com/IBM-Swift/SwiftyJSON", majorVersion: 17, minor: 0),
       .package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 1, minor: 0)
+  ]
+  targets:      [
+      .target(name: "SwiftClickHouse", dependencies: ["SwiftyJson", "BlueSocket"]),
   ]
 )
