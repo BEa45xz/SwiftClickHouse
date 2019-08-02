217090af85fe581c6ac460a8afeb33498e8b5511
BEa45xz <49711174+BEa45xz@users.noreply.github.com>
2019-08-02 13:07:03 +0200

    Update Package.swift
    minor major is deprecated

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
   name:         "SwiftClickHouse",
   dependencies: [
-      .package(url: "https://github.com/IBM-Swift/SwiftyJSON", majorVersion: 17, minor: 0),
-      .package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 1, minor: 0)
+      .package(url: "https://github.com/IBM-Swift/SwiftyJSON", from: "17.0.0"),
+      .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.0")
   ]
   targets:      [
       .target(name: "SwiftClickHouse", dependencies: ["SwiftyJson", "BlueSocket"]),
