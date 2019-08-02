e4723bc85ea9d8b34d4ae1acdbe880787b788b55
BEa45xz <49711174+BEa45xz@users.noreply.github.com>
2019-08-02 13:07:40 +0200

    Update Package.swift
    syntax error

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   dependencies: [
       .package(url: "https://github.com/IBM-Swift/SwiftyJSON", from: "17.0.0"),
       .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.0")
-  ]
+  ],
   targets:      [
       .target(name: "SwiftClickHouse", dependencies: ["SwiftyJson", "BlueSocket"]),
   ]
